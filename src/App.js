9c11ab63bfe6857b8b53b7cb46479883174249c6
brindhasuvarna <31232077@vupune.ac.in>
2025-10-03 00:43:54 +0530

    Add App.js with React Router setup and wrap with Redux Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import LandingPage from './components/LandingPage';
 import ProductListing from './components/ProductListing';
 import ShoppingCart from './components/ShoppingCart';
 import Header from './components/Header';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
   return (
