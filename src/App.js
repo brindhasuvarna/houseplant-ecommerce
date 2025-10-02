17b8ae2c1ecf20d371c991f0d476ced32038e451
brindhasuvarna <31232077@vupune.ac.in>
2025-10-03 00:03:16 +0530

    Add App.js with routing and Redux provider

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import LandingPage from './components/LandingPage';
+import ProductListing from './components/ProductListing';
+import ShoppingCart from './components/ShoppingCart';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
